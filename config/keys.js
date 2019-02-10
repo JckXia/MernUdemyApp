742e06d99c9f809488482c6fcfbd5aeada628e4d
JackXiaisagreatguy <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-02-10 18:10:07 -0500

    Setting up production

--- config/keys.js
+++ config/keys.js
@@ -1,5 +1,8 @@
-module.exports={
-  googleClientID:'1085958123232-vn2mii2v0h46t65v78593bt8hbgnpsij.apps.googleusercontent.com',
-  googleClientSecret:'APkyMGmzZe0KscifIQl10dj8',
-  cookieKey: 'indasjdNSd421$SDssssma$s'
-};
+ //keys.js needs to figure out what set of credentials to return
+if(process.env.NODE_ENV == 'production'){
+  //We are in production, return prod keys
+  module.exports=require('./prod');
+}else{
+  //We are in development, return dev keys
+  module.exports=require('./dev');
+}
