e92d8f1a4ab79383cbd3cbd42c57bf0e8a7683e0
JackXiaisagreatguy <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-02-10 13:45:55 -0500

    Adding Oauth

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const express=require('express');
+const mongoose=require('mongoose');
+const cookieSession=require('cookie-session');
+const passport=require('passport');
+const keys=require('./config/keys');
 const app=express();
 
-app.get('/',(req,res)=>{
-   res.send({hi:'There'});
-});
-const PORT = process.env.PORT || 3000;
+//App.use hooks up the middleware
+//Modify incoming request before they are sent to the backend route handlers
+app.use(
+  cookieSession({
+    maxAge:30*24*60*60*1000,
+    keys:[keys.cookieKey]
+  })
+);
+app.use(passport.initialize());
+app.use(passport.session());
+
+require('./models/User');
+require('./services/passport'); //file inside passpVAort is executed
+
+var MongoDBPort=process.env.MONGODB_URI || "mongodb://localhost:27017/UdemyMERNApp";
+mongoose.connect(MongoDBPort);
+require('./routes/authRoutes')(app);
+
+const PORT = process.env.PORT || 5000;
 app.listen(PORT,function(){
   console.log('Listening on port');
 });
---
+++ routes/authRoutes.js
@@ -0,0 +1,23 @@
+//User logins to google here
+const passport=require('passport');
+
+module.exports=(app)=>{
+  //Exporting functions from tis file
+app.get('/auth/google',passport.authenticate('google',{
+  scope:['profile','email']
+}));
+
+//User vists this route. Strategy sees that it has the code in the url
+app.get('/auth/google/callback',passport.authenticate('google'));
+
+app.get('/api/current_user',(req,res)=>{
+    res.send(req.user);
+});
+
+app.get('/api/logout',(req,res)=>{
+  //Takes the cookie which contains user id, and kills it
+    req.logout();
+    res.send(req.user);
+});
+
+};
