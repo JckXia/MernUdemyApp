343ab3707235e3469414f46aea9abb05c3ad302e
JackXiaisagreatguy <31197636+JackXiaisagreatguy@users.noreply.github.com>
2019-02-10 17:43:13 -0500

    Delete later

---
+++ config/keys.js
@@ -0,0 +1,5 @@
+module.exports={
+  googleClientID:'1085958123232-vn2mii2v0h46t65v78593bt8hbgnpsij.apps.googleusercontent.com',
+  googleClientSecret:'APkyMGmzZe0KscifIQl10dj8',
+  cookieKey: 'indasjdNSd421$SDssssma$s'
+};
